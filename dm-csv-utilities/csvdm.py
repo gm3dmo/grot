7bdfdbb97e3aaa17baf4fcade1455ecbb2c8a4b2
David Morris <gm3dmo@gmail.com>
2019-07-06 15:34:13 +0100

    Adding a scrambling function to obscure data.

--- dm-csv-utilities/csvdm.py
+++ dm-csv-utilities/csvdm.py
@@ -14,7 +14,7 @@ def openCSVFile(f):
     return csv_dict
 
 
-def summarizeCSV(csv_dict, num_rows_to_summarize=1, separator=' = '):
+def summarizeCSV(csv_dict, num_rows_to_summarize=1, separator=' = ', scramble=True):
     """Generate a view of each column in a csv file printed line by line:
 
 fruit,count
@@ -40,11 +40,31 @@ count = 2
         if csv_dict.line_num >= num_rows_to_summarize:
             break
         else:
+            if scramble == True:
+                things_to_scramble = [ 'Day' ]
+                row = scrambleValues(things_to_scramble, row)
             for item in row:
                 l = (f'{item}{separator}{hl_on}{row[item]}{hl_off}')
                 print(l)
             logging.debug(f"""line number of csv: {csv_dict.line_num}""")
             print()
+
+
+def lowercaseList(list):
+     """convert the items in a list to lowercase"""
+     return [x.lower() for x in list]
+
+
+def scrambleValues(columns_to_scramble, row, replacement_value='*****'):
+    """Takes a list of column headings that need to be scrambled and replaces the valued in them with """
+    logging.debug(f"scrambling: {columns_to_scramble}")
+    columns_to_scramble = lowercaseList(columns_to_scramble)
+    logging.debug(columns_to_scramble)
+    for column in row:
+        if column.lower() in columns_to_scramble:
+            row[column]  = replacement_value
+            logging.debug(f"scrambling {column}")
+    return row
 
 
 if __name__ == "__main__":
