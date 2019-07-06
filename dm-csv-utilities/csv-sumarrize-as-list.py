d397da09c9cb0dd68ecb3bb682fb89c75c5a9df7
David Morris <gm3dmo@gmail.com>
2019-07-06 07:16:39 +0100

    csv report test data

--- dm-csv-utilities/csv-sumarrize-as-list.py
+++ dm-csv-utilities/csv-sumarrize-as-list.py
@@ -10,15 +10,16 @@ def main():
   f = sys.argv[1]
   infile = csv.DictReader(open(f))
 
+  number_of_records = 10
 
   for row in infile:
      for item in row:
         if sys.stdout.isatty():
-            print(f'{item} = \033[7m\033[31m{row[item]}\033[0m')
+            l = (f'{item} = \033[7m\033[31m{row[item]}\033[0m')
         else:
-            print(f'{item} = {row[item]}')
+            l = (f'{item} = {row[item]}')
+        print(l)
 
-     print()
 
 if __name__ == "__main__":
     main()
