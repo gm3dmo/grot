bbf7e40b64f80e12eaf5b83af37071a85d7009dc
David Morris <gm3dmo@gmail.com>
2019-07-06 07:46:39 +0100

    it's nice to have a separate function.

--- dm-csv-utilities/csv-sumarrize-as-list.py
+++ dm-csv-utilities/csv-sumarrize-as-list.py
@@ -4,21 +4,40 @@ import os
 import sys
 import csv
 
+def openCSVFile(f):
+    csv_dict = csv.DictReader(open(f))
+    print(dir(csv_dict))
+    print(csv_dict.fieldnames)
+    print(csv_dict.dialect)
+    print(csv_dict.line_num)
+    print(csv_dict.reader)
+    print(csv_dict.restkey)
+    print(csv_dict.restval)
+    return csv_dict
+
 
 def main():
 
   f = sys.argv[1]
-  infile = csv.DictReader(open(f))
+  csv_dict = openCSVFile(f)
 
-  number_of_records = 10
+  num_rows_to_summarize = 10
 
-  for row in infile:
-     for item in row:
-        if sys.stdout.isatty():
-            l = (f'{item} = \033[7m\033[31m{row[item]}\033[0m')
-        else:
-            l = (f'{item} = {row[item]}')
-        print(l)
+  if sys.stdout.isatty():
+      hl_on  = """\033[7m\033[31m"""
+      hl_off = """\033[0m')"""
+  else:
+      hl_on = ""
+      hl_off = ""
+
+  for row in csv_dict:
+     if csv_dict.line_num >= num_rows_to_summarize:
+         break
+     else:
+        for item in row:
+            l = (f'{item} = {hl_on}{row[item]}{hl_off}')
+            print(l)
+        print()
 
 
 if __name__ == "__main__":
