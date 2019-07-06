f450aef7b8d6bde5c4dad2062226865bf5eb11a6
David Morris <gm3dmo@gmail.com>
2019-07-06 08:01:43 +0100

    now we have some debug logging for when things go danish.

--- dm-csv-utilities/csv-sumarrize-as-list.py
+++ dm-csv-utilities/csv-sumarrize-as-list.py
@@ -3,21 +3,19 @@
 import os
 import sys
 import csv
+import logging
 
 def openCSVFile(f):
     csv_dict = csv.DictReader(open(f))
-    print(dir(csv_dict))
-    print(csv_dict.fieldnames)
-    print(csv_dict.dialect)
-    print(csv_dict.line_num)
-    print(csv_dict.reader)
-    print(csv_dict.restkey)
-    print(csv_dict.restval)
+    #logging.debug(dir(csv_dict))
+    logging.debug(f"""CSV Dialect: {csv_dict.dialect}""")
+    logging.debug(f"""Field names: {csv_dict.fieldnames}""")
     return csv_dict
 
 
 def main():
 
+  logging.basicConfig(level=logging.DEBUG)
   f = sys.argv[1]
   csv_dict = openCSVFile(f)
 
@@ -37,6 +35,7 @@ def main():
         for item in row:
             l = (f'{item} = {hl_on}{row[item]}{hl_off}')
             print(l)
+        logging.debug(f"""line number of csv: {csv_dict.line_num}""")
         print()
 
 
