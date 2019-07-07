37786666158b86f94f3ba797e26f5a0e6fd765a6
David Morris <gm3dmo@gmail.com>
2019-07-07 19:22:32 +0100

    ran black on this file.

--- dm-csv-utilities/summarize-simple.py
+++ dm-csv-utilities/summarize-simple.py
@@ -3,22 +3,24 @@
 import sys
 import csv
 
+
 def main():
 
-  f = sys.argv[1]
-  infile = csv.DictReader(open(f))
+    f = sys.argv[1]
+    infile = csv.DictReader(open(f))
 
-  count = 0
-  for row in infile:
-     if count >= 1:
-         break
-     for item in row:
-        if sys.stdout.isatty():
-            print(f'{item} = \033[7m\033[31m({row[item]})\033[0m')
-        else:
-            print(f'({item}) = ({row[item]})')
-     print()
-     count += 1
+    count = 0
+    for row in infile:
+        if count >= 1:
+            break
+        for item in row:
+            if sys.stdout.isatty():
+                print(f"{item} = \033[7m\033[31m({row[item]})\033[0m")
+            else:
+                print(f"({item}) = ({row[item]})")
+        print()
+        count += 1
+
 
 if __name__ == "__main__":
     main()
